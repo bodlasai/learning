58480f26e73a017d71ba27d9225be4d1afc1ad96
bodla sai <30978046+bodlasai@users.noreply.github.com>
2022-04-14 18:11:26 +0530

    testdemo

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-    backend "s3" {
-        bucket ="s3backendfiles"
-        key="remotestate1.tfstate"
-        region="us-east-2"
-    }
-}
+# terraform {
+#     backend "s3" {
+#         bucket ="s3backendfiles"
+#         key="remotestate1.tfstate"
+#         region="us-east-2"
+#     }
+# }
